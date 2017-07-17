97039936a9090cc624e0897225ed029d917bd889
Olivera Mauricio <feboxp@gmail.com>
2017-07-17 20:07:40 -0300

    - Nueva pantalla de inicio. - Cambios en el color background

---
+++ application/views/welcome.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>Soluciones Empresariales</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Archivo+Black" rel="stylesheet">
+  </head>
+ <style>
+    html {
+    margin: 0px;
+    height: 100%;
+    width: 100%;
+    }
+
+    body {
+    margin: 0px;
+    min-height: 100%;
+    width: 100%;
+    background-color: #fbfaf5 !important;
+    }
+
+    .content {
+        
+        text-align:center;
+        margin-top:20%;
+        color: #2B2B2B;
+    }
+    .content h1{
+        font-family: 'Archivo Black', sans-serif;
+        font-size: 10.9vh;
+    }
+    .content h2{
+        font-family: 'Archivo Black', sans-serif;
+        font-size: 3.0vh;
+    }
+
+    .content .btns{
+        font-weight: 500;
+        color: #2B2B2B;
+        border-color: #2B2B2B;
+        text-decoration: none;
+    }
+
+    </style>
+  <body>
+    <div class="content">
+        <h1>SE</h1>
+        <h2>Soluciones Empresariales</h2>
+        <a href="login" class="btn btn-bs btns">Ingresar</a>
+    </div>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
