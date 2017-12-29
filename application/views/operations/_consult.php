eb07ea275437d8c37d9eec725b5219bb8e0e3976
Sergio Moyano <sergio.moyano@outlook.com.ar>
2017-12-29 09:18:08 -0300

    center column

--- application/views/operations/_consult.php
+++ application/views/operations/_consult.php
@@ -26,11 +26,19 @@
 	<div class="col-xs-12">
   		<label style="text-decoration: underline"> Valores Tomados </label><br>
   		<table width="100%" class="table table-bordered table-hover datatable">
-			<tr style="text-align: center"><th>Banco</th><th>Número</th><th>Librador</th><th>Fecha Pago</th><th>Tasa</th><th>Días</th><th>Importe</th></tr>
+			<tr style="text-align: center">
+        <th style="text-align: center">Banco</th>
+        <th style="text-align: center">Número</th>
+        <th style="text-align: center">Librador</th>
+        <th style="text-align: center">Fecha Pago</th>
+        <th style="text-align: center">Tasa</th>
+        <th style="text-align: center">Días</th>
+        <th style="text-align: center">Importe</th>
+      </tr>
 			<tr>
-			<td><?php echo $data['banco']['razon_social'];?></td>
+			<td style="text-align: center"><?php echo $data['banco']['razon_social'];?></td>
 			<td style="text-align: center"><?php echo $data['operation']['nro_cheque'];?></td>
-			<td><?php echo $data['emisor']['apellido'] . ' ' . $data['emisor']['nombre'];?></td>
+			<td style="text-align: center"><?php echo $data['emisor']['apellido'] . ' ' . $data['emisor']['nombre'];?></td>
 			<td style="text-align: center"><?php echo date("d-m-Y", strtotime($data['operation']['fecha_venc']));?></td>
 			<td style="text-align: center"><?php echo number_format($data['operation']['tasa_mensual'], 2, ',', '.');?></td>
 			<td style="text-align: center"><?php echo number_format($data['operation']['nro_dias'], 0, ',', '.');?></td>
@@ -64,11 +72,16 @@
   		?>
   				<label style="text-decoration: underline"> Cheques </label><br>
   				<table width="100%" class="table table-bordered table-hover datatable">
-  					<tr style="text-align: center"><th>Banco</th><th>Número</th><th>Importe</th><th>Fecha</th></tr>
+  					<tr style="text-align: center">
+              <th style="text-align: center">Banco</th>
+              <th style="text-align: center">Número</th>
+              <th style="text-align: center">Importe</th>
+              <th style="text-align: center">Fecha</th>
+            </tr>
   				<?php
   				foreach ($data['cheques'] as $che) {
   					echo '<tr>';
-					echo  	'<td>'.$che[0].'</td>';
+					echo  	'<td style="text-align: center">'.$che[0].'</td>';
 					echo  	'<td style="text-align: center">'.$che[1].'</td>';
 					echo  	'<td style="text-align: center">'.$che[2].'</td>';
 					echo  	'<td style="text-align: center">'.date("d-m-Y", strtotime($che[3])).'</td>';
@@ -84,11 +97,16 @@
   		?>
   				<label style="text-decoration: underline"> Transferencias </label><br>
   				<table width="100%" class="table table-bordered table-hover datatable">
-  				<tr style="text-align: center"><th>Banco</th><th>Número</th><th>Importe</th><th>Fecha</th></tr>
+  				<tr style="text-align: center">
+            <th style="text-align: center">Banco</th>
+            <th style="text-align: center">Número</th>
+            <th style="text-align: center">Importe</th>
+            <th style="text-align: center">Fecha</th>
+          </tr>
   				<?php
   				foreach ($data['transferencias'] as $che) {
   					echo '<tr>';
-					echo  	'<td>'.$che[0].'</td>';
+					echo  	'<td style="text-align: center">'.$che[0].'</td>';
 					echo  	'<td style="text-align: center">'.$che[1].'</td>';
 					echo  	'<td style="text-align: center">'.$che[2].'</td>';
 					echo  	'<td style="text-align: center">'.date("d-m-Y", strtotime($che[3])).'</td>';
